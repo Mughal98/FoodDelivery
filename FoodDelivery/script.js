183692b82127f9bcc4b8be539cb1d888973e9b09
Qasim Mughal <93707939+Mughal98@users.noreply.github.com>
2025-03-10 21:12:54 +0500

    v0.5 DONE
    GG

--- FoodDelivery/script.js
+++ FoodDelivery/script.js
@@ -67,7 +67,7 @@ function displayCuisines() {
 // Add scroll event listener to change header color
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
-  if (window.scrollY > 50) {
+  if (window.scrollY > 370) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
